76ddc6b640529811a52d9172be1057bb1d761875
Erix-glitch <71719838+Erix-glitch@users.noreply.github.com>
2025-10-20 19:29:38 -0700

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from rtlsdr import RtlSdr
+
+sdr = RtlSdr()
+
+# configure device
+sdr.sample_rate = 2.048e6  # Hz
+sdr.center_freq = 70e6     # Hz
+sdr.freq_correction = 0
+sdr.gain = 'auto'
+
+print(sdr.read_samples(512))
